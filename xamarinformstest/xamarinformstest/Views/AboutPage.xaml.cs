fce35a2513f262c459648ac242d35ae85536d683
yinyue200 <yinyue200@outlook.com>
2020-03-09 22:35:53 +0800

    Add project files.

---
+++ xamarinformstest/xamarinformstest/Views/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinformstest.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            var assembly = typeof(AboutPage).Assembly;
+            logo.Source = ImageSource.FromStream(() =>
+            {
+                return assembly.GetManifestResourceStream("xamarinformstest.v2-adc33f3b65032caefd1155d4a8438564_r.jpg");
+            });
+        }
+    }
+}
